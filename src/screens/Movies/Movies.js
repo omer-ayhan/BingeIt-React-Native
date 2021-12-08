9420a05537f5d5788dad2d65a73a21d15bcf221e
salik-a <salika@mef.edu.tr>
2021-12-09 01:47:48 +0300

    moviecard updated and data fetched on mavies screen

--- src/screens/Movies/Movies.js
+++ src/screens/Movies/Movies.js
@@ -1,14 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
 import styles from "./MoviesStyle"
-
+import axios from "axios";
+import MovieCard from "../../components/MovieCard";
 
 const Movies = () => {
 
+    const [movieData, setMovieData] = useState();
+    const [loading, setLoading] = useState(true);
 
+    const fetchData = async () => {
+        try {
+            const { data } = await axios.get("http://10.0.2.2:3000/movies");
+            setMovieData(data)
+            setLoading(false);
+        } catch (error) {
+            console.log(error)
+        }
+
+    };
+
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const handleCardSelect = (url) => {
+        navigation.navigate('EpisodePage', { url })
+    };
+
+    if (loading) {
+        return (
+            <View>
+                <Text>Loading</Text>
+            </View>
+        )
+    }
     return (
         <View>
-            <Text>Movies</Text>
+
+            <FlatList
+                renderItem={({ item }) => <MovieCard data={item} onSelect={() => handleCardSelect(item)} />}
+                data={movieData}
+                keyExtractor={item => item.id}
+                numColumns={"1"}
+            />
         </View>
     )
 };
