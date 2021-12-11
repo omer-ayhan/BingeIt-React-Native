d480763ac6efc8e366996e73d7f84ab7c8cb8d32
Ömer Ayhan <om.ayhan247@gmail.com>
2021-12-11 22:45:46 +0300

    style: :lipstick: Movie Screen
    - added color variables object for reusability

--- src/screens/Movies/Movies.js
+++ src/screens/Movies/Movies.js
@@ -1,64 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, FlatList } from "react-native";
-import styles from "./MoviesStyle"
+import styles from "./MoviesStyle";
 import axios from "axios";
 import MovieCard from "../../components/MovieCard";
 import FloatingButton from "../../components/FilterButton";
 import ModalCard from "../../components/ModalCard";
 const Movies = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [movieData, setMovieData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [genre, setGenre] = useState("");
 
-    const [modalVisible, setModalVisible] = useState(false)
-    const [movieData, setMovieData] = useState();
-    const [loading, setLoading] = useState(true);
-    const [genre, setGenre] = useState("");
-
-
-    const fetchData = async () => {
-        try {
-            const { data } = await axios.get(`http://10.0.2.2:3000/movies?genre_like=${genre}`);
-            setMovieData(data)
-            setLoading(false);
-        } catch (error) {
-            console.log(error)
-        }
-
-    };
-
-
-    useEffect(() => {
-        fetchData()
-    }, [genre]);
-
-    const handleCardSelect = (item) => {
-        navigation.navigate('Detail', { item })
-    };
-    function handleModalVisible() {
-        setModalVisible(!modalVisible);
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://192.168.1.124:3000/movies?genre_like=${genre}`
+      );
+      setMovieData(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    if (loading) {
-        return (
-            <View>
-                <Text>Loading</Text>
-            </View>
-        )
-    }
+  useEffect(() => {
+    fetchData();
+  }, [genre]);
+
+  const handleCardSelect = (item) => {
+    navigation.navigate("Detail", { item });
+  };
+  function handleModalVisible() {
+    setModalVisible(!modalVisible);
+  }
+
+  if (loading) {
     return (
-        <View style={styles.container}>
-
-            <FlatList
-                renderItem={({ item }) => <MovieCard data={item} onSelect={() => handleCardSelect(item)} />}
-                data={movieData}
-                keyExtractor={item => item.id}
-            />
-            <ModalCard
-                visible={modalVisible}
-                onClose={handleModalVisible}
-                onSelect={setGenre}
-            />
-            <FloatingButton iconName="plus" onPress={handleModalVisible} />
-        </View>
-    )
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <FlatList
+        renderItem={({ item }) => (
+          <MovieCard data={item} onSelect={() => handleCardSelect(item)} />
+        )}
+        data={movieData}
+        keyExtractor={(item) => item.id}
+      />
+      <ModalCard
+        visible={modalVisible}
+        onClose={handleModalVisible}
+        onSelect={setGenre}
+      />
+      <FloatingButton iconName="plus" onPress={handleModalVisible} />
+    </View>
+  );
 };
 
 export default Movies;
---
+++ src/style/colors.js
@@ -0,0 +1,7 @@
+export default {
+  main: "#FB8C00",
+  mainDark: "#DF9300",
+  yellow: "#FBD300",
+  dark: "#1C1C1C",
+  gray: "#767676",
+};
